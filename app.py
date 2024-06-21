2d21fdf2c80e8ed516acf816f95323b2bba10c48
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-06-21 14:10:24 +0000

    added sd-3

---
+++ app.py
@@ -0,0 +1,23 @@
+from diffusers import StableDiffusion3Pipeline
+import torch
+from io import BytesIO
+import base64
+# import os
+
+class InferlessPythonModel:
+    def initialize(self):
+        # HF_TOKEN =  os.getenv("HF_TOKEN") # Access Hugging Face token from environment variable
+        self.pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers", torch_dtype=torch.float16,token="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")
+        self.pipe = self.pipe.to("cuda")
+        
+    def infer(self, inputs):
+        prompt = inputs["prompt"]
+        image = self.pipe(prompt,negative_prompt="low quality",num_inference_steps=28,guidance_scale=7.0,).images[0]
+        buff = BytesIO()
+        image.save(buff, format="JPEG")
+        img_str = base64.b64encode(buff.getvalue()).decode()
+
+        return {"generated_image_base64" : img_str }
+    
+    def finalize(self):
+        self.pipe = None
